61aa1066c67264af121fbfd3f2f39f174e8745e4
Nicholas Roy <nicholas@patroy.ca>
2025-04-12 18:38:46 -0400

    firebase functionality
    Untested, added firebase connection in app/build, project/build, pubspec.yaml,
    
    Formatted project into Views and Models folders

--- habittracker/lib/main.dart
+++ habittracker/lib/main.dart
@@ -1,7 +1,23 @@
 import 'package:flutter/material.dart';
-import 'splash_screen.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'Views/splash_screen.dart';
 
-void main() {
+//in json "package_name"
+//"package_name": "com.android.habitTracker"
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // initialize the firebase
+  await Firebase.initializeApp(
+      options: FirebaseOptions(
+          apiKey: "AIzaSyCs7QYbh0cUdMRuTpnDl-VDmL9GC1HFH48",
+          appId: "17345865914",
+          messagingSenderId: "17345865914",
+          projectId: "habittracker-5c882"
+      )
+  );
+
   runApp(const MyApp());
 }
 
