da1313296f37d8c286e7cfb3798e91ff3cc30717
Nicholas Roy <nicholas@patroy.ca>
2025-04-18 23:51:39 -0400

    minor commenting

--- habittracker/lib/main.dart
+++ habittracker/lib/main.dart
@@ -1,43 +1,46 @@
-// import 'package:flutter/material.dart';
-// import 'package:cloud_firestore/cloud_firestore.dart';
-// import 'package:firebase_core/firebase_core.dart';
-// import 'Views/splash_screen.dart';
-//
-// //in json "package_name"
-// //"package_name": "com.android.habitTracker"
-//
-// void main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   // initialize the firebase
-//   await Firebase.initializeApp(
-//       options: FirebaseOptions(
-//           apiKey: "AIzaSyCs7QYbh0cUdMRuTpnDl-VDmL9GC1HFH48",
-//           appId: "17345865914",
-//           messagingSenderId: "17345865914",
-//           projectId: "habittracker-5c882"
-//       )
-//   );
-//
-//   runApp(const MyApp());
-// }
-//
-// class MyApp extends StatelessWidget {
-//   const MyApp({Key? key}) : super(key: key);
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Habit Tracker',
-//       debugShowCheckedModeBanner: false,
-//       theme: ThemeData(
-//         primarySwatch: Colors.blue,
-//         useMaterial3: true,
-//       ),
-//       home: const SplashScreen(),
-//     );
-//   }
-// }
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'Views/splash_screen.dart';
 
+//in json "package_name"
+//"package_name": "com.android.habitTracker"
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // initialize the firebase
+  await Firebase.initializeApp(
+      options: FirebaseOptions(
+          apiKey: "AIzaSyCs7QYbh0cUdMRuTpnDl-VDmL9GC1HFH48",
+          appId: "17345865914",
+          messagingSenderId: "17345865914",
+          projectId: "habittracker-5c882"
+      )
+  );
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Habit Tracker',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        useMaterial3: true,
+      ),
+      home: const SplashScreen(),
+    );
+  }
+}
+
+
+//WHat does this do???
+/*
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -158,3 +161,4 @@ class _MainPageState extends State<MainPage> {
     );
   }
 }
+*/
